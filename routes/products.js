8ae6d993ff778bf71748ebcf5290def1e6c4b1b7
Akash <yadavzak@amazon.com>
2020-07-18 05:11:11 +0530

    Modified product api

--- routes/products.js
+++ routes/products.js
@@ -1,14 +1,38 @@
-const express = require('express');
-const router = express.Router()
-const fetch = require('node-fetch');
+const express = require("express");
+const router = express.Router();
+const fetch = require("node-fetch");
 
-router.get('/list', async (req, res) => {
-  let response = await fetch('https://developers.zomato.com/api/v2.1/categories', {
-    method: 'get',
-    headers: { 'user-key': process.env.API_KEY }
+router.get("/list", async (req, res) => {
+  let res_id = [
+    18494129,
+    18674420,
+    18531910,
+    18735687,
+    18357341,
+    39692,
+    36267,
+    36272,
+    16538641,
+  ];
+  let ar = [];
+  for (let i = 0; i < res_id.length; i++) {
+    let querystring =
+      "https://developers.zomato.com/api/v2.1/restaurant?res_id=" + res_id[i];
+    let promise = fetch(querystring, {
+      method: "get",
+      headers: { "user-key": process.env.API_KEY },
+    })
+      .then((response) => response.json())
+      .catch((err) => console.log(err));
+    ar.push(promise);
+  }
+  Promise.all(ar).then((results) => {
+    let arr = [];
+    for (let i = 0; i < results.length; i++) {
+      arr.push(results[i]);
+    }
+    res.json({ restaurants: arr });
   });
-  let data = await response.json();
-  res.json(data);
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,18 +1,20 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const connectDB = require('./db');
-const cors = require('cors');
-const userRouter = require('./routes/User');
+const connectDB = require("./db");
+const cors = require("cors");
+const userRouter = require("./routes/User");
 const port = process.env.PORT || 8080;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-app.use('/orders', require('./routes/Order'));
-app.use('/users', userRouter);
-app.use('/products', require('./routes/products'))
+app.use("/orders", require("./routes/Order"));
+app.use("/users", userRouter);
+app.use("/products", require("./routes/products"));
 
-app.get('/', (req, res) => res.send('Welcome to API'));
+app.get("/", (req, res) => res.send("Welcome to API"));
 
-connectDB(() => app.listen(port, () => console.log(`Listening on ${port} port`)));
+connectDB(() =>
+  app.listen(port, () => console.log(`Listening on ${port} port`))
+);
